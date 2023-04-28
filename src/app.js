1af01af4548cab2b998122cfd3653ccdf3f91363
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2023-04-28 06:43:18 +0000

    First Attemt

--- src/app.js
+++ src/app.js
@@ -6,6 +6,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = [
+    "My dog",
+    "My wife",
+    "Darth Vader",
+    "Professor Snape",
+    "The Police"
+  ];
+  const what = [
+    "took a poop",
+    "forced me to take out the garbage",
+    "force choked me",
+    "used the killing curse on my cat",
+    "arrested me"
+  ];
+  const where = [
+    "in my bed.",
+    "at Disney World.",
+    "at a five star restaurant.",
+    "while swimming the English channel.",
+    "in Fiji.",
+    "on the Deathstar.",
+    "at Hogwarts."
+  ];
+
+  let sentence =
+    who[rand(who.length)] + // each array's index will be a random number from
+    " " +
+    what[rand(what.length)] +
+    " " +
+    where[rand(where.length)];
+
+  let excuseSpan = document.querySelector("#excuse");
+
+  excuseSpan.innerHTML = sentence;
 };
+
+function rand(length) {
+  // Given the length, randomize a number between 0 and the length of the array.
+  let number = Math.floor(Math.random() * length);
+  return number; // return the new randomized number
+}
